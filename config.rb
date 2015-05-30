96740dd80ee055950c6d8add79e291b721131528
Cole Willsea <coleww@gmail.com>
2015-05-30 11:51:13 -0700

    delete tags, add header image

--- config.rb
+++ config.rb
@@ -20,7 +20,7 @@ activate :blog do |blog|
   # blog.day_link = "{year}/{month}/{day}.html"
   # blog.default_extension = ".markdown"
 
-  blog.tag_template = "tag.html"
+  # blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
 
   # Enable pagination
