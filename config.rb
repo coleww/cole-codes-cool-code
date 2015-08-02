51832928cf8379ed8766198686985056b2b0faae
Cole Willsea <coleww@gmail.com>
2015-08-02 08:43:25 -0700

    idk

--- config.rb
+++ config.rb
@@ -31,39 +31,6 @@ end
 
 page "/feed.xml", layout: false
 
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", layout: false
-#
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
 
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
@@ -112,4 +79,4 @@ end
 
 activate :meta_tags
 set_meta_tags description: 'Making art with code and vice versa.'
-set_meta_tags keywords: %w(ruby javascript code clojure npm algorithm art).join(', ')
+set_meta_tags keywords: %w(ruby javascript code poetry clojure npm algorithm art).join(', ')
